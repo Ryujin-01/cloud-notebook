d28b3249a3b82e9d57af7e465af750e43266b0c1
Ryujin <ryujinunofficial01@gmail.com>
2025-10-02 21:19:47 +0530

    Development of backend

---
+++ backend/routes/auth.js
@@ -0,0 +1,53 @@
+// Importing all the necessary modules
+const express = require("express");
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
+const router = express.Router();
+
+router.post(
+  // Creating a user using POST: "/api/auth/createuser". No login required
+  "/createuser",
+
+  // Checking for errors.
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 8 characters").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req); // Taking errors.
+
+    // If errors are present, displaying errors.
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Else creating user.
+    try {
+      // Check whether with the same email already exists,
+      let user = await User.findOne({ email: req.body.email.toLowerCase() });
+      console.log(user);
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "User with this email already exists !" });
+      }
+      // If user doen't exist, create new user.
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password, // hash this later
+      });
+      console.log("New account created !");
+      res.json(user);
+    } catch (err) {
+      // If some error occured
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
